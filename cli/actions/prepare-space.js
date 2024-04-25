695fca56f3e80e6363b07e07812124b216b1c87c
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 18:27:44 +0100

    wip

--- cli/actions/prepare-space.js
+++ cli/actions/prepare-space.js
@@ -13,7 +13,6 @@ import { $ } from "zx"
         let res
         await $`mkdir -p ~/.ssh`
         await $`echo ${key} > ~/.ssh/id_rsa ; chmod 600 ~/.ssh/id_rsa`
-        await $`ssh-keygen -R ${host}`
         await $`touch ~/.ssh/known_hosts ; chmod 600 ~/.ssh/known_hosts`
         await $`ssh-keyscan -p ${port} ${host} >> ~/.ssh/known_hosts`
 
