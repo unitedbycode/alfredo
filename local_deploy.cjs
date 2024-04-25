785fc3d9629fd585bd2b9e7ccf9e6aecc6abd0a4
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 09:25:55 +0100

    wip

--- local_deploy.cjs
+++ local_deploy.cjs
@@ -1,26 +1,61 @@
 const fs = require('fs')
 const path = '/Users/joaopatricio/code/mcs/intra24/.github/workflows/staging.yml'
-const $ = require("zx")
+const $ = import("zx")
+const simpleGit = require('simple-git')
+const semver = require('semver')
 
-// Read existing content from file
-fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-        console.error(`Error reading from ${path}: `, err)
-        return
+// Create a Simple Git instance pointing to local repo
+const git = simpleGit()
+
+async function bumpVersion() {
+
+    const tagStrings = (await git.tag()).split('\n').filter(Boolean)
+
+    let maxTagSemVerString
+
+    for(const tag of tagStrings) {
+        if(semver.valid(tag)) {  // Check if it is valid SemVer format.
+            if(!maxTagSemVerString || semver.gt(tag, maxTagSemVerString)) {  // If current version > maximum found version,
+                maxTagSemVerString=tag                                     // then set as new maximum found version.
+            }
+        } else{
+            // console.log(`Skipping invalid SemVar ${tag}`)
+        }
     }
+    // console.log("Current Version: ",maxTagSemVerString)
 
-    // Replace 'XXX' with 'YYY'
-    const result = data.replace(/alfredo@v0.0.8/g, 'YYY')
+    return semver.inc(maxTagSemVerString,'patch')
+}
 
-    // Write new content back into same file
-    fs.writeFile(path, result,'utf8', err => {
-        if(err){
-            console.error(`Error writing to ${path}: `, err)
+const replaceInFile = (path, search, replace) => {
+
+    // Read existing content from file
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading from ${path}: `, err)
             return
         }
 
-        console.log('File has been updated successfully.')
-    })
-})
+        // Replace 'XXX' with 'YYY'
+        const result = data.replace(search, replace)
 
+        // Write new content back into same file
+        fs.writeFile(path, result,'utf8', err => {
+            if(err){
+                console.error(`Error writing to ${path}: `, err)
+                return
+            }
+
+            console.log('File has been updated successfully.')
+        })
+    })
+}
+
+bumpVersion()
+    .then(nextVersion => {
+        $`git tag v${nextVersion}`
+        $`git push --tags`
+        replaceInFile(path, /(alfredo@v)(\d+\.\d+\.\d+)/g, nextVersion)
+    })
+    .catch(console.error.bind(console))
 
