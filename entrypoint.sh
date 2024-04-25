69934fccc44330678437f6ea81f10ca5cb8a909e
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 18:56:25 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,9 @@ cat ~/.ssh/id_rsa
 touch ~/.ssh/known_hosts ; chmod 600 ~/.ssh/known_hosts
 ssh-keyscan -p $INPUT_PORT $INPUT_HOST >> ~/.ssh/known_hosts
 
+SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+ssh $SSH_OPTIONS -i ~/.ssh/id_rsa -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "ls -alh"
 
 cd /src ; npm run start -- $@
 
