1bb8d9d0416178b78dd1030a3d4621c10a0f5a83
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 12:22:47 +0100

    wip

--- cli/alfredo.js
+++ cli/alfredo.js
@@ -28,12 +28,16 @@ async function execute(cmd) {
         const foo = await $`whoami`
         console.log(foo.toString())
 
+        const runner_workspace = process.env.RUNNER_WORKSPACE
+        const github_workspace = process.env.GITHUB_WORKSPACE
 
         await execute("pwd")
-        await execute("ls -alh")
+
+        await execute(`ls ${runner_workspace}`)
+        await execute(`ls ${github_workspace}`)
 
         // print where this file is being executed
-        console.log(process)
+        console.log(process.env)
         console.log(process.cwd())
 
         const __filename = fileURLToPath(import.meta.url)
@@ -43,15 +47,20 @@ async function execute(cmd) {
         console.log("__filename:", __filename) // Should print the directory containing this script.
         console.log("__dirname:", __dirname) // Should print the directory containing this script.
 
-        await execute("ansible --help")
+        // await execute("ansible --help")
 
         // Fetch the value from input 'who-to-greet' specified in action.yml file
         const nameToGreet = core.getInput("who-to-greet")
         console.log(`Hello ${nameToGreet}!`)
 
+        // Fetch the value from input 'who-to-greet' specified in action.yml file
+        const nameToGreet2 = core.getInput("WHO-TO-GREET")
+        console.log(`Hello2 ${nameToGreet2}!`)
+
         // Record time when greeting was done as part of outputs
         const time = new Date().toTimeString()
         core.setOutput("time", time)
+
     } catch (error) {
         // Handle errors properly by setting failure status on error occurrence
         core.setFailed(error.message)
