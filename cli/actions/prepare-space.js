a88077ce0269526a1191b07984750f92c8535b07
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 18:01:05 +0100

    wip

--- cli/actions/prepare-space.js
+++ cli/actions/prepare-space.js
@@ -1,7 +1,5 @@
 import core from "@actions/core"
 import { $ } from "zx"
-import { fileURLToPath } from "url"
-import { dirname } from "path"
 
 // Main action execution flow wrapped in an async IIFE (Immediately Invoked Function Expression)
 (async () => {
@@ -9,6 +7,17 @@ import { dirname } from "path"
         // Fetch the value from input 'who-to-greet' specified in action.yml file
         const nameToGreet2 = core.getInput("who_to_greet")
         console.log(`Hello2 ${nameToGreet2}!`)
+
+        let res
+        res = await $`mkdir -p ~/.ssh`
+
+        res = await $`echo ${process.env.key} > ~/.ssh/id_rsa`
+        console.log(res.toString())
+
+        res = await $`cat ~/.ssh/id_rsa`
+        console.log(res.toString())
+
+
 
         // Record time when greeting was done as part of outputs
         const time = new Date().toTimeString()
