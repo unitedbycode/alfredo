f1ee36a39df3d94dbdbb20a58593c9280dd38fe9
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 19:07:05 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 mkdir ~/.ssh
+touch ~/.ssh/known_hosts ; chmod 600 ~/.ssh/known_hosts
+ssh-keyscan -p $INPUT_PORT $INPUT_HOST >> ~/.ssh/known_hosts
+
+cat ~/.ssh/known_hosts
 
 # If $INPUT_KEY is set, write it to a file
 if [ -n "$INPUT_KEY" ]; then
@@ -11,9 +15,12 @@ fi
 ls -alh ~/.ssh
 cat ~/.ssh/id_rsa
 
-touch ~/.ssh/known_hosts ; chmod 600 ~/.ssh/known_hosts
-ssh-keyscan -p $INPUT_PORT $INPUT_HOST >> ~/.ssh/known_hosts
+SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
+echo "[command]With options...."
+ssh $SSH_OPTIONS -i ~/.ssh/id_rsa -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "ls -alh"
+
+echo "[command]WITHOUT options...."
 ssh -i ~/.ssh/id_rsa -p $INPUT_PORT $INPUT_USERNAME@$INPUT_HOST "ls -alh"
 
 cd /src ; npm run start -- $@
