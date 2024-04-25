13ba6bdaa5bbdbba6c351e30ec714dc857dca5a7
Joao Patricio <ijpatricio@gmail.com>
2024-04-25 18:10:34 +0100

    wip

--- cli/actions/prepare-space.js
+++ cli/actions/prepare-space.js
@@ -5,19 +5,27 @@ import { $ } from "zx"
 (async () => {
     try {
         // Fetch the value from input 'who-to-greet' specified in action.yml file
-        const nameToGreet2 = core.getInput("who_to_greet")
-        console.log(`Hello2 ${nameToGreet2}!`)
+        const host = core.getInput('host')
+        const port = core.getInput('port')
+        const username = core.getInput('username')
+        const key = core.getInput('key')
 
         let res
-        res = await $`mkdir -p ~/.ssh`
+        await $`mkdir -p ~/.ssh`
+        await $`echo ${key} > ~/.ssh/id_rsa`
+        await $`chmod 600 ~/.ssh/id_rsa`
+        await $`ssh-keyscan -i ~/.ssh/id_rsa -p ${port} ${host} >> ~/.ssh/known_hosts`
 
-        res = await $`echo ${process.env.key} > ~/.ssh/id_rsa`
+        res = await $`ssh -i ~/.ssh/id_rsa -p ${port} ${username}@${host} '''
+        ls -alh
+        ls -alh spaces
+        pwd
+        whoami
+        '''`
         console.log(res.toString())
 
-        res = await $`cat ~/.ssh/id_rsa`
-        console.log(res.toString())
-
-
+        // res = await $`cat ~/.ssh/id_rsa`
+        // console.log(res.toString())
 
         // Record time when greeting was done as part of outputs
         const time = new Date().toTimeString()
