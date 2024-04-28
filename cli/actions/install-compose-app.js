67d51cd420ee0368991388ec6a2a189968943f85
Joao Patricio <ijpatricio@gmail.com>
2024-04-28 08:28:28 +0100

    wip

--- cli/actions/install-compose-app.js
+++ cli/actions/install-compose-app.js
@@ -2,6 +2,7 @@ import core from '@actions/core'
 import exec from '@actions/exec'
 import { startCommandGreetings, validateInputs } from '../utils/common.js'
 import { prepareUserKey, privateKeyPath } from '../utils/system-ssh-connection.js'
+import { $, cd } from 'zx'
 
 const task = async () => {
     await startCommandGreetings('Installing Docker Compose App...')
@@ -14,6 +15,36 @@ const task = async () => {
         validateInputs([])
 
         prepareUserKey()
+
+        let out
+        out = await $`
+        cd /github/workspace
+        docker run --rm -v $PWD:/app -w /app composer bash -c "pwd ; whoami; ls -alh"
+        `
+        core.info(out)
+
+        cd('/src/ansible')
+
+        return
+        // await exec.exec(`ls -alh /github/workspace`)
+
+        await exec.exec('cp', ['-R', '/github/workspace', '/tmp/app'])
+        await exec.exec(`ls -alh /tmp/app/.alfredo/scripts`)
+        await exec.exec('install.sh', [], { cwd: '/tmp/app/.alfredo/scripts' })
+
+        return
+
+        const projectPath = '/github/workspace'
+        await exec.exec(
+            `docker run --rm -v /tmp/app:/app -w /app composer install --ignore-platform-reqs --no-scripts`,
+            [],
+            { cwd: projectPath },
+        )
+
+        return
+        await exec.exec(`cd /github/workspace`)
+        await exec.exec(`ls -alh`)
+        await exec.exec(`ls -alh`)
 
         const user = core.getInput('username')
 
--- cli/local_run.sh
+++ cli/local_run.sh
@@ -20,11 +20,24 @@ if [ -n "$INPUT_KEY_BASE64" ]; then
     export INPUT_KEY=$(echo "$INPUT_KEY_BASE64" | base64 -d)
 fi
 
+## Locally get a repository like if it was the the checkout of a repository
+cd $DEPLOY_DIR
+git archive -o bench_app_archive.tar.gz --format=tar.gz main
+cd -
+mv $DEPLOY_DIR/bench_app_archive.tar.gz .
+rm -rf bench_app
+mkdir -p bench_app
+tar xvf bench_app_archive.tar.gz -C ./bench_app
+
+
+
 echo "Running task: $1"
 
 docker run --rm \
     -it \
     -v $(pwd):/src \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd)/bench_app:/github/workspace \
     -v ./entrypoint.sh:/entrypoint.sh \
     --entrypoint="/entrypoint.sh" \
     -w /src \
